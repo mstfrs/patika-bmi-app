96dff1ff30c0a7887c101093fa601e33bef3da4b
mstfrs <mstfrs68@gmail.com>
2022-07-30 16:58:26 +0300

    form added

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,36 +1,88 @@
 import {
-  FormControl,
-  Input,
-  FormHelperText,
-  InputLabel,
-  Container,
-  TextField,
+  
+  Button,
+  Container, Icon, InputAdornment, TextField,
+  
 } from "@mui/material";
-import React from "react";
+import SendIcon from '@mui/icons-material/Send';
+
+import {useState} from "react";
 
 const Calculator = () => {
+  const [user, setUser] = useState({name:"",weight:"",height:""})
+
+
+function handleChangeName(e) {
+  
+  setUser({...user,name:e.target.value})
+  
+  
+}
+function handleChangeWeight(e) {
+  
+  setUser({...user,weight:e.target.value})
+  
+  
+}
+function handleChangeHeight(e) {
+  
+  setUser({...user,height:e.target.value})
+  
+  
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(user)
+  
+}
   return (
-    <div>
-      <Container maxWidth="sm" style={{ height: "100vh", padding: "20px" }}>
-        <FormControl sx={{ display: "flex", color: "white" }}>
-          <TextField
-            id="name-input"
-            label="Your Name"
-            type="text"
-            variant="filled"
-            placeholder="Mustafa"
-            sx={{ color: "white" }}
-          />
-          <br />
-          <TextField
-            id="name-input"
-            label="Your Name"
-            type="text"
-            variant="filled"
-            placeholder="Mustafa"
-            sx={{ color: "white" }}
-          />
-        </FormControl>
+    <div style={{width:"100vh", paddingBottom:"5rem"}}>
+      <Container maxWidth="sm" style={{ height: "40vh", width:"20rem",padding: "20px", backgroundColor:"white" , marginTop :"50px"}}>
+        
+
+<form onSubmit={handleSubmit}>
+<TextField
+          label="Your Name"
+          id="outlined-start-adornment"
+          // className={clsx(classes.margin, classes.textField)}
+          
+          variant="outlined"
+          onChange={handleChangeName}
+        /><br/><br/><br/>
+<TextField
+          label="Your Weight"
+          id="outlined-start-adornment"
+          // className={clsx(classes.margin, classes.textField)}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">Kg</InputAdornment>,
+          }}
+          variant="outlined"
+          onChange={handleChangeWeight}
+        /><br/><br/><br/>
+<TextField
+          label="Your Height"
+          id="outlined-start-adornment"
+          // className={clsx(classes.margin, classes.textField)}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">Cm</InputAdornment>,
+          }}
+          variant="outlined"
+          onChange={handleChangeHeight}
+        /><br/><br/><br/>
+        
+        <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        // className={classes.button}
+        endIcon={<SendIcon/>}
+      >
+        Calculate
+      </Button>
+     
+        {/* <input type="submit" value="Submit" /> */}
+      </form>
       </Container>
     </div>
   );
